ff4c69ea94b4fdc12679bf51814d5ff7540a90f3
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2022-05-15 19:08:36 +0600

    Server runs

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
-import Express from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://tanvirnoor38:tanvirnoor38@cluster0.1h0vl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlparser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
