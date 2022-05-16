ecac5bb631be476e534c2b387aaaae598623f0f9
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2022-05-17 00:15:45 +0600

    Redux Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from "react-redux";
+import { getPosts } from './actions/posts';
 import memories from './images/memories1.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return (
         <Container maxidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
