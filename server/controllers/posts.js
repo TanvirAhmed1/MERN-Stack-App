7a082c96da09ab16e35af1c0fcc8b88f16b05694
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2022-05-17 12:21:16 +0600

    FullMERNprojectworks

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,13 +8,26 @@ export const getPosts = async (req, res) => {
         res.status(404).json({message: error.message});
     }
 }
+// export const createPost = async (req, res) => {
+//     const post = req.body;
+//     const newPost = new PostMessage(post);
+//     try{
+//         await newPost.Save();
+//         res.status(201).json(newPost);
+//     }catch (error){
+//         res.status(409).json({message: error.message});
+//     }
+// }
 export const createPost = async (req, res) => {
-    const post = req.body;
-    const newPost = new PostMessage(post);
-    try{
-        await newPost.Save();
-        res.status(201).json(newPost);
-    }catch (error){
-        res.status(409).json({message: error.message});
+    const { title, message, selectedFile, creator, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
     }
 }
